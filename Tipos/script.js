c6f2f3e63ce81dd04071cba2fc77bd39de1bbd60
Marcelo Joia <sitejoia@hotmail.com>
2023-07-01 18:47:21 -0300

    Tipos do javaScript

---
+++ Tipos/script.js
@@ -0,0 +1,23 @@
+const a = 10
+const b = 10.5
+const c = 'Maria'
+const d = 'a'
+const e = false
+const f = {name: 'Maria', age: 25}
+const g = ['a', 15, false, 'maria']
+const h = null
+const i = undefined
+
+console.log("Imprime o Valor: " + a);//=> Imprime o Valor
+console.log("Imprime o Tipo: " + typeof a); //=> Imprime o Tipo
+
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+console.log("const f: "+typeof f)
+console.log("const f: "+typeof g)
+console.log(typeof h)
+console.log(typeof i)
