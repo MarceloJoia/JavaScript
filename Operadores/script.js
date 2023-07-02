443d2801eec0d91cce3993467d8e65eef88fb40c
Marcelo Joia <sitejoia@hotmail.com>
2023-07-02 10:55:02 -0300

    Estruturas de controle

--- Operadores/script.js
+++ Operadores/script.js
@@ -1,56 +1,52 @@
-console.log("Operadores aritméticos [ + - * / % ** (exponenciação ES2016) ]");
-console.log(`3 + 4 = ${3 + 4}`);
-console.log(`3 - 4 = ${3 - 4}`);
-console.log(`3 * 4 = ${3 * 4}`);
-console.log(`19 / 3 = ${19 / 5}`);
-console.log(`19 % 3 = ${19 % 5}`);
-console.log(`3 ** 4 = ${3 ** 4}`);
+const time = 19;
 
-console.log('');
-console.log("Operadores comparativos [ < > <= >= ]");
-console.log(`10 > 5 = ${10 > 5}`);
-console.log(`10 < 5 = ${10 < 5}`);
-console.log(`10 >= 5 = ${10 >= 5}`);
-console.log(`10 <= 5 = ${10 <= 5}`);
+if (time < 12) {
+    console.log("Bom dia");
+} else if (time < 18) {
+    console.log("Boa tarde");
+} else {
+    console.log("Boa noite");
+}
 
+const week = 3;
+switch (week) {
+    case 1:
+        console.log("Domingo");
+        break;
+    case 2:
+        console.log("Segunda");
+        break;
+    case 3:
+        console.log("Terça");
+        break;
+    case 4:
+        console.log("Quarta");
+        break;
+    case 5:
+        console.log("Quinta");
+        break;
+    case 6:
+        console.log("Sexta");
+        break;
+    case 7:
+        console.log("Sábado");
+        break;
+    default:
+        console.log("Valor inválido");
+}
 
+let count = 4;
+while (count > 0) {
+    console.log(`COUNT = ${count}`);
+    count--;
+}
 
-console.log('');
-console.log('');
-console.log("--> Operadores comparativos de igualdade <--");
-console.log('Converte para um tipo em comum, depois compara: [ == != ] ==> ${"" == false}');
-console.log(`"" == false: ${"" == false}`);
+for (let i = 0; i < 4; i++) {
+    console.log(`I = ${i}`);
+}
 
-console.log('');
-console.log('Compara levando em conta também o tipo: [ === !== ] ==> ${"" === false}');
-console.log(`"" === false: ${"" === false}`);
-
-console.log('');
-console.log('object: compara referência: [ string, number, boolean: compara valor ] ==> ${"17" == 17} e ${"17" === 17}');
-console.log(`"17" == 17: ${"17" == 17}`);
-console.log(`"17" === 17: ${"17" === 17}`);
-
-
-console.log('');
-console.log("Operadores comparativos de igualdade");
-const x = "Maria";
-const y = "Maria";
-const a = new String("Maria");//=> Função construtorab
-
-const b = new String("Maria");
-console.log(`const x = "Maria"`);
-console.log(`const y = "Maria"`);
-console.log(`const a = new String("Maria")`);
-console.log(`const b = new String("Maria"`);
-console.log(`x === y: ${x === y}`);
-console.log(`a === b: ${a === b}`);
-console.log(`typeof x: ${typeof x}`);
-console.log(`typeof a: ${typeof a}`);
-
-console.log('');
-console.log("Operadores lógicos [&& (E) || (OU) !(NÃO)]");
-console.log(`(10 > 5) && (10 > 20): ${(10 > 5) && (10 > 20)}`);
-console.log(`(10 > 5) && (10 < 20): ${(10 > 5) && (10 < 20)}`);
-console.log(`(10 > 5) || (10 > 20): ${(10 > 5) || (10 > 20)}`);
-console.log(`(10 < 5) || (10 > 20): ${(10 > 5) && (10 > 20)}`);
-console.log(`!(10 > 5): ${!(10 > 5)}`);
+let test = 4;
+do {
+    console.log(`TEST = ${test}`);
+    test--;
+} while (test > 0);
